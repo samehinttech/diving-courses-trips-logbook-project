b07966f838b5e5a67449dc87eb6cf37d57286b22
sameh <sameh.ahmed@students.fhnw.ch>
2025-04-22 17:29:59 +0200

    New Project data

---
+++ oceandive/src/main/java/com/oceandive/OceandiveApplication.java
@@ -0,0 +1,13 @@
+package com.oceandive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceandiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceandiveApplication.class, args);
+	}
+
+}
---
+++ oceandive/src/test/java/com/oceandive/OceandiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceandive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceandiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
