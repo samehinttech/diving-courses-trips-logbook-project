3ad9265a5219e5357494b8a1db6e9367f2a09c0e
sameh <sameh.ahmed@students.fhnw.ch>
2025-05-03 01:35:49 +0200

    03/05/2025 Succesful mvn clean install 😊

--- src/main/java/ch/fhnw/oceandive/model/activity/DiveLog.java
+++ src/main/java/ch/fhnw/oceandive/model/activity/DiveLog.java
@@ -1,5 +1,6 @@
 package ch.fhnw.oceandive.model.activity;
 
+import ch.fhnw.oceandive.model.user.UserEntity;
 import jakarta.persistence.*;
 
 
@@ -10,11 +11,25 @@ public class DiveLog {
   @GeneratedValue
   private Long id;
 
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private UserEntity user;
+
+
+
+
   public void setId(Long id) {
     this.id = id;
   }
 
   public Long getId() {
     return id;
+  }
+  public UserEntity getUser() {
+    return user;
+  }
+  public void setUser(UserEntity user) {
+    this.user = user;
   }
 }
