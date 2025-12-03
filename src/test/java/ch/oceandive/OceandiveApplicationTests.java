d6aace5c70e15dbb7d72b329e7bb7ba5c3fb83f6
Sameh <samehinttech@gmail.com>
2025-12-03 22:13:43 +0100

    fix: rebase

--- src/test/java/ch/oceandive/OceandiveApplicationTests.java
+++ src/test/java/ch/oceandive/OceandiveApplicationTests.java
@@ -2,7 +2,6 @@ package ch.oceandive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class OceandiveApplicationTests {
